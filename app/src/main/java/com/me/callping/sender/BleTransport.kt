15ad15c992769297951c038d32f17525dbe072bf
Michail19 <Mi.ershov.2017@yandex.ru>
2026-01-11 23:10:30 +0300

    Added funs

--- app/src/main/java/com/me/callping/sender/BleTransport.kt
+++ app/src/main/java/com/me/callping/sender/BleTransport.kt
@@ -2,10 +2,13 @@ package com.me.callping.sender
 
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
+import android.bluetooth.le.AdvertiseData
+import android.bluetooth.le.AdvertiseSettings
 import android.bluetooth.le.BluetoothLeAdvertiser
 import android.content.Context
 import android.util.Log
 import com.me.callping.model.CallEvent
+import java.util.UUID
 
 class BleTransport (
     private val context: Context
@@ -35,11 +38,32 @@ class BleTransport (
     }
 
     override fun send(event: CallEvent) {
-        TODO("Not yet implemented")
+        val advertiser = advertiser ?: return
+
+        val settings = AdvertiseSettings.Builder()
+            .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY)
+            .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_LOW)
+            .setConnectable(false)
+            .build()
+
+        val data = AdvertiseData.Builder()
+            .addServiceUuid(SERVICE_UUID)
+            .addManufacturerData(
+                MANUFACTURER_ID,
+                encodeEvent(event)
+            )
+            .setIncludeDeviceName(false)
+            .build()
+
+        advertiser.startAdvertising(settings, data, advertiseCallback)
+
+        Log.d(TAG, "Advertising CallEvent: $event")
     }
 
     override fun stop() {
-        TODO("Not yet implemented")
+        advertiser?.stopAdvertising(advertiseCallback)
+        advertiser = null
+        Log.d(TAG, "BLE advertise stopped")
     }
 
     companion object {
