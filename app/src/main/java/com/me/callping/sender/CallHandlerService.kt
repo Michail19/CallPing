df64bede507e3680b015edced7293362be7c34a4
Michail19 <Mi.ershov.2017@yandex.ru>
2026-01-11 20:19:59 +0300

    Try with Build.VERSION.SDK

--- app/src/main/java/com/me/callping/sender/CallHandlerService.kt
+++ app/src/main/java/com/me/callping/sender/CallHandlerService.kt
@@ -1,4 +1,29 @@
 package com.me.callping.sender
 
-class CallHandlerService {
+import android.os.Build
+import android.telecom.Call
+import android.telecom.CallScreeningService
+import android.util.Log
+
+class CallHandlerService : CallScreeningService() {
+
+    override fun onScreenCall(callDetails: Call.Details) {
+
+        // Проверка на входящий звонок
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val direction = call.getDetails().getCallDirection();
+            if (direction == Call.Details.DIRECTION_INCOMING) {
+                return
+            }
+        }
+        else {
+            if (callDetails.callDirection != Call.Details.DIRECTION_INCOMING) {
+                return
+            }
+        }
+    }
+
+    companion object {
+        private const val TAG = "CallHandlerService"
+    }
 }
