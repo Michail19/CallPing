73b5188bdf0f24f1b4514be74b4757f7e513c26e
Michail19 <Mi.ershov.2017@yandex.ru>
2026-01-12 22:47:47 +0300

    Listener added

--- app/src/main/java/com/me/callping/listener/ListenerService.kt
+++ app/src/main/java/com/me/callping/listener/ListenerService.kt
@@ -1,4 +1,38 @@
 package com.me.callping.listener
 
-class ListenerService {
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+
+class ListenerService : Service() {
+
+    private lateinit var bleServer: BleServer
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startForeground(
+            NOTIFICATION_ID,
+            NotificationController.createServiceNotification(this)
+        )
+
+        bleServer = BleServer(
+            context = applicationContext,
+            eventHandler = IncomingEventHandler(applicationContext)
+        )
+
+        bleServer.start()
+    }
+
+    override fun onDestroy() {
+        bleServer.stop()
+        super.onDestroy()
+    }
+
+    override fun onBind(intent: Intent?): IBinder? = null
+
+    companion object {
+        private const val NOTIFICATION_ID = 1001
+    }
 }
