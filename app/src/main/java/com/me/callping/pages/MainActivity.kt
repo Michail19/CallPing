9bb290c0abea45487f69f78ee5d807ae6f8f9dba
Michail19 <Mi.ershov.2017@yandex.ru>
2026-01-13 23:35:07 +0300

    Beta 0.1.1

--- app/src/main/java/com/me/callping/pages/MainActivity.kt
+++ app/src/main/java/com/me/callping/pages/MainActivity.kt
@@ -2,6 +2,7 @@ package com.me.callping.pages
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.me.callping.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val navView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        navView.setupWithNavController(navController)
     }
 }
