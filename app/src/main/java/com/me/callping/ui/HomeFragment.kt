f76e2ed26ae1673cbab58433492b1502b54e27fb
Michail19 <Mi.ershov.2017@yandex.ru>
2026-01-15 00:00:52 +0300

    Big update

---
+++ app/src/main/java/com/me/callping/ui/HomeFragment.kt
@@ -0,0 +1,33 @@
+package com.me.callping.ui
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.me.callping.R
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.scanQrButton)
+            .setOnClickListener {
+                findNavController()
+                    .navigate(R.id.action_home_to_scanQr)
+            }
+
+        view.findViewById<Button>(R.id.devicesButton)
+            .setOnClickListener {
+                findNavController()
+                    .navigate(R.id.action_home_to_devices)
+            }
+
+        view.findViewById<Button>(R.id.showQrButton)
+            .setOnClickListener {
+                findNavController()
+                    .navigate(R.id.action_home_to_generateQr)
+            }
+    }
+}
