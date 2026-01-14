deddd352d4bbd10eafbfe034fcd119bb14df581a
Michail19 <Mi.ershov.2017@yandex.ru>
2026-01-14 23:00:49 +0300

    States and generate

--- app/src/main/java/com/me/callping/ui/pairing/generate/GenerateQrState.kt
+++ app/src/main/java/com/me/callping/ui/pairing/generate/GenerateQrState.kt
@@ -1,4 +1,9 @@
 package com.me.callping.ui.pairing.generate
 
-class GenerateQrState {
+import android.graphics.Bitmap
+
+sealed class GenerateQrState {
+    object Loading : GenerateQrState()
+    data class Ready(val bitmap: Bitmap) : GenerateQrState()
+    data class Error(val message: String) : GenerateQrState()
 }
--- app/src/main/java/com/me/callping/ui/pairing/generate/GenerateQrViewModel.kt
+++ app/src/main/java/com/me/callping/ui/pairing/generate/GenerateQrViewModel.kt
@@ -1,4 +1,35 @@
 package com.me.callping.ui.pairing.generate
 
-class GenerateQrViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.me.callping.tools.QrBitmapGenerator
+import com.me.callping.tools.QrPayloadBuilder
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class GenerateQrViewModel(
+    private val payloadBuilder: QrPayloadBuilder, // = QrPayloadBuilder(),
+    private val qrGenerator: QrBitmapGenerator // = QrBitmapGenerator()
+) : ViewModel() {
+
+    private val _state = MutableStateFlow<GenerateQrState>(GenerateQrState.Loading)
+    val state: StateFlow<GenerateQrState> = _state
+
+    init {
+        generateQR()
+    }
+
+    private fun generateQR() {
+        viewModelScope.launch {
+            try {
+                val payload = payloadBuilder.build()
+                val bitmap = qrGenerator.generate(payload)
+
+                _state.value = GenerateQrState.Ready(bitmap)
+            } catch (e: Exception) {
+                _state.value = GenerateQrState.Error(e.message ?: "QR generation failed")
+            }
+        }
+    }
 }
