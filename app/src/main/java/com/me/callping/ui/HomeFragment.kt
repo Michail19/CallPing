d16f1c25ac84714c205e1d9c3609c140a4e96125
Michail19 <Mi.ershov.2017@yandex.ru>
2026-01-15 21:49:13 +0300

    Comment extra button

--- app/src/main/java/com/me/callping/ui/HomeFragment.kt
+++ app/src/main/java/com/me/callping/ui/HomeFragment.kt
@@ -12,11 +12,11 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.scanQrButton)
-            .setOnClickListener {
-                findNavController()
-                    .navigate(R.id.action_home_to_scanQr)
-            }
+//        view.findViewById<Button>(R.id.scanQrButton)
+//            .setOnClickListener {
+//                findNavController()
+//                    .navigate(R.id.action_home_to_scanQr)
+//            }
 
         view.findViewById<Button>(R.id.devicesButton)
             .setOnClickListener {
