91baf2df096b9063009d76114b1bb4dbf8d5ca7a
Michail19 <Mi.ershov.2017@yandex.ru>
2026-01-17 14:17:09 +0300

    Update

--- app/src/main/java/com/me/callping/ui/MainActivity.kt
+++ app/src/main/java/com/me/callping/ui/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.me.callping.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.me.callping.R
+import com.me.callping.service.ListenerService
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, ListenerService::class.java)
+
+        startForegroundService(intent)
     }
 }
